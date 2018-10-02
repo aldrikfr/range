85629a0fa3129d51a4620fc71f99b842d04d8f03
Aldrik KLEBER <contact@aldrik.net>
2018-10-02 15:14:39 +0200

    add of internal function implode

--- range.ml
+++ range.ml
@@ -42,3 +42,5 @@ the Free Software Foundation, either version 3 of the License, or
 
   let aggregage f {start_a;stop_a} {start_b;stop_b} =
     {start = (f start_a start_b) ; stop = (f stop_a stop_b)}
+
+  let implode f {start;stop} = f start stop
