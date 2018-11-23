7586d9609412664bc962beb102b697b5deefe162
Aldrik KLEBER <contact@aldrik.net>
2018-11-23 15:41:27 +0100

    improvement of from documentation
    * improvement of esthetic
    * add of some ocamldoc fields

--- range.mli
+++ range.mli
@@ -11,15 +11,23 @@ sequence described by a start value and a stop value. This module provide
 functions to fold this range, and some basic set operation likes cross and
 join.
 The main goal is to provide a split capacity in order to make life easy for
-distributed processing. *)
+distributed processing.
+
+@author Aldrik KLEBER
+
+*)
 
 (** t type correspond to a integer range value *)
 type t
 
-(**
-from int_value -> int_value will create a t value representing the range
+(** from int_value -> int_value will create a t value representing the range
 described by the two values given in parameter.
-Usage from 2 3 and from 3 2 are equivalent *)
+Usage from 2 3 and from 3 2 are equivalent
+
+@param start : int representating the starting value of the range
+@param stop : int representing the last value of the range
+@return range : t the corresponding range type value defined by start and stop
+ *)
 val from : int -> int -> t
 
 (** fold f acc_init range_value result
