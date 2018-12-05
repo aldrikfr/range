a7fe95cb0b4357f5bdbe42971b5f3142a4dc027d
Aldrik KLEBER <contact@aldrik.net>
2018-12-05 15:36:23 +0100

    ocamlformat

--- src/range.ml
+++ src/range.ml
@@ -7,7 +7,8 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version. *)
 
 type range_record = {start: int; stop: int}
-    (*
+
+(*
 type range_modification = Filter of (int -> bool)
 *)
 type t =
@@ -22,28 +23,23 @@ let implode f p =
 
 let from start stop = Natural {start= min start stop; stop= max start stop}
 
-let bind f = function
-  | None -> None
-  | Some n -> f n
+let bind f = function None -> None | Some n -> f n
 
 let filter f = function
   | Natural r ->
-    let modifier = bind (fun n -> if f n then Some n else None) in
-    Modified (r,modifier)
+      let modifier = bind (fun n -> if f n then Some n else None) in
+      Modified (r, modifier)
   | Modified (r, f_prev) ->
-    let modifier x =
-      x
-      |> f_prev
-      |> bind (fun n -> if f n then Some n else None) in
-    Modified (r,modifier)
+      let modifier x =
+        x |> f_prev |> bind (fun n -> if f n then Some n else None)
+      in
+      Modified (r, modifier)
 
 let filtered_from start stop f_filter = from start stop |> filter f_filter
 
 let is_filtered = function Natural _ -> false | Modified _ -> true
 
-let reset = function
-  | Natural r -> Natural r
-  | Modified (r, _) -> Natural r
+let reset = function Natural r -> Natural r | Modified (r, _) -> Natural r
 
 let rec fold_by_loop {start; stop} step f acc n =
   if n > stop then acc
@@ -53,11 +49,10 @@ let rec fold_by_loop {start; stop} step f acc n =
 let fold_by step f acc = function
   | Natural r -> fold_by_loop r step f acc r.start
   | Modified (r, f_filter) ->
-    let f_with_filter acc n =
-      match f_filter (Some n) with
-      | Some mn -> f acc mn
-      | None -> acc in
-    fold_by_loop r step f_with_filter acc r.start
+      let f_with_filter acc n =
+        match f_filter (Some n) with Some mn -> f acc mn | None -> acc
+      in
+      fold_by_loop r step f_with_filter acc r.start
 
 let rec fold_loop {start; stop} f acc n =
   if n > stop then acc else fold_loop {start; stop} f (f acc n) (succ n)
@@ -65,11 +60,10 @@ let rec fold_loop {start; stop} f acc n =
 let fold f acc = function
   | Natural r -> fold_loop r f acc r.start
   | Modified (r, f_filter) ->
-    let f_agg acc n =
-      match f_filter (Some n) with
-      | Some mn ->  f acc mn
-      | None -> acc in
-    fold_loop r f_agg acc r.start
+      let f_agg acc n =
+        match f_filter (Some n) with Some mn -> f acc mn | None -> acc
+      in
+      fold_loop r f_agg acc r.start
 
 let rec iter_loop {start; stop} f n =
   if n > stop then ()
@@ -80,11 +74,10 @@ let rec iter_loop {start; stop} f n =
 let iter f = function
   | Natural r -> iter_loop r f r.start
   | Modified (r, f_filter) ->
-    let f_with_filter n =
-      match f_filter (Some n) with
-      | Some mn -> f mn
-      | None -> () in
-    iter_loop r f_with_filter r.start
+      let f_with_filter n =
+        match f_filter (Some n) with Some mn -> f mn | None -> ()
+      in
+      iter_loop r f_with_filter r.start
 
 let length = implode (fun start stop -> stop - start)
 
@@ -106,14 +99,11 @@ let split minimal n r =
 let contain e = function
   | Natural r -> r.start <= e && e <= r.stop
   | Modified _ as data ->
-    fold (fun acc n -> if n = e then true else acc) false data
-
+      fold (fun acc n -> if n = e then true else acc) false data
 
 let get_range_record_from = function Modified (r, _) -> r | Natural r -> r
 
-let handle_result_with_exception = function
-  | Ok r -> r
-  | Error m -> failwith m
+let handle_result_with_exception = function Ok r -> r | Error m -> failwith m
 
 let cross a b =
   let ra = get_range_record_from a in
@@ -133,14 +123,11 @@ let join_exn a b = join a b |> handle_result_with_exception
 
 let map f = function
   | Natural r ->
-    let modifier = bind (fun n -> Some (f n)) in
-    Modified (r,modifier)
+      let modifier = bind (fun n -> Some (f n)) in
+      Modified (r, modifier)
   | Modified (r, f_filter) ->
-    let modifier mn =
-      mn
-      |> f_filter
-      |> bind (fun n -> Some (f n)) in
-    Modified (r,modifier)
+      let modifier mn = mn |> f_filter |> bind (fun n -> Some (f n)) in
+      Modified (r, modifier)
 
 let aggregate f a b =
   let ra = get_range_record_from a in
--- src/range.mli
+++ src/range.mli
@@ -21,6 +21,7 @@ distributed processing.
 (** t type correspond to a integer range value *)
 type t
 
+val from : int -> int -> t
 (** from int_value -> int_value will create a t value representing the range
     described by the two values given in parameter.
 
@@ -29,8 +30,8 @@ type t
     @return Range.t type which value defined by start and stop parameters
 
  *)
-val from : int -> int -> t
 
+val filter : (int -> bool) -> t -> t
 (** filter on predicate function
     attach a predicate that will modify the behaviour of iter or fold funtions
     in order to apply only to values that satisfies the predicate.
@@ -41,8 +42,8 @@ val from : int -> int -> t
     range value will merge the two filters.
     @return new Range.t value with a new filter added.
     **)
-val filter : (int -> bool) -> t -> t
 
+val filtered_from : int -> int -> (int -> bool) -> t
 (** create a new range of integer with a filters
 
     @param start Integer representating the starting value of the range
@@ -51,23 +52,23 @@ val filter : (int -> bool) -> t -> t
     must respect the signature int -> bool
     @return new Range.t type which value defined by start and stop parameters
     **)
-val filtered_from : int -> int -> (int -> bool) -> t
 
+val reset : t -> t
 (** remove all map and filter effects from a range.
 
     @param old Range.t value
     @return new Range.t value from parameter without modifiers.
    **)
-val reset : t -> t
 
+val is_filtered : t -> bool
 (** is filtered predicate
 
     test if a Range.t value contain a filter or not.
     @param Range.t value to test
     @return test true if there is a filter false otherwise
    **)
-val is_filtered : t -> bool
 
+val fold : ('a -> int -> 'a) -> 'a -> t -> 'a
 (** fold the equivalent of List.fold_left applied to integer range_record
     explore all the values contained in the rang value applying f to the
     accumulator and the current element read by fold. If a filter was associated
@@ -78,8 +79,8 @@ val is_filtered : t -> bool
     @param acc initial value of the accumulator
     @param range explored range value
     @return value of the accumulator after reading all elements **)
-val fold : ('a -> int -> 'a) -> 'a -> t -> 'a
 
+val iter : (int -> unit) -> t -> unit
 (** iter apply a function with side effect on all values of the range. This
     function support filtering.
 
@@ -87,8 +88,8 @@ val fold : ('a -> int -> 'a) -> 'a -> t -> 'a
     @param range value
     @return unit
    **)
-val iter : (int -> unit) -> t -> unit
 
+val split : int -> int -> t -> t list
 (** split a range value into a list of smaller range, useful for batching in
     parallel processing.
 
@@ -98,16 +99,16 @@ val iter : (int -> unit) -> t -> unit
     @return list of ranges with a size of minimal or greater, the list having
     count elements max.
     **)
-val split : int -> int -> t -> t list
 
+val contain : int -> t -> bool
 (** contain function to test if an integer value is contained in a Range.t values
 
     @param element to be tested
     @param reference range value
     @result true if element is contained in reference
     **)
-val contain : int -> t -> bool
 
+val cross : t -> t -> (t, string) result
 (** new Range.t value representing the common value between two Range.t values.
 
     @param a Range.t value
@@ -115,12 +116,12 @@ val contain : int -> t -> bool
     @return Result type with a Range.t value defined by the common values, and
     Error string message if it is impossible to find common values.
     **)
-val cross : t -> t -> (t, string) result
 
+val cross_exn : t -> t -> t
 (** Same as cross function with exception for error handling.
    **)
-val cross_exn : t -> t -> t
 
+val join : t -> t -> (t, string) result
 (** Join to generate a new Range.t value contained both a and b
 
     @param a Range.t value
@@ -129,12 +130,12 @@ val cross_exn : t -> t -> t
     If a and b are disjoint, they can't be joinded so an Error string message
     is returned.
    **)
-val join : t -> t -> (t, string) result
 
+val join_exn : t -> t -> t
 (** Same as join with exception for error handling
    **)
-val join_exn : t -> t -> t
 
+val map : (int -> int) -> t -> t
 (** apply f to limits value of Range.t value
 
     This feature is in DEVELOPMENT and is INSTABLE
@@ -143,8 +144,6 @@ val join_exn : t -> t -> t
     @param r range to modify
     @return updated range
    **)
-val map : (int -> int) -> t -> t
-
 
 val aggregate : (int -> int -> int) -> t -> t -> t
 
