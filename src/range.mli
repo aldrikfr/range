e934fa3cc7f0b895b210dd55a9f2bebfbdffc474
Aldrik KLEBER <contact@aldrik.net>
2019-11-20 15:37:12 +0100

    small improvement of documentation

--- src/range.mli
+++ src/range.mli
@@ -76,9 +76,9 @@ val fold : ('a -> elt -> 'a) -> 'a -> t -> 'a
     @return value of the accumulator after reading all elements *)
 
 val fold_right : ('a -> elt -> 'a) -> 'a -> t -> 'a
-(** fold_right explore all the values contained in the range value, in reverse order, 
-    starting by the last value to the first one, applying f to the accumulator and 
-    the current element read by fold. If a filter was associated to the range value, 
+(** fold_right explore all the values contained in the range value, in reverse order,
+    starting by the last value to the first one, applying f to the accumulator and
+    the current element read by fold. If a filter was associated to the range value,
     only element validated by the predicate f will be passed to the function.
 
     @param f function aggregating the accumulator to the current value.
